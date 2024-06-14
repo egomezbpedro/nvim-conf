156efb2ac5e3947b8963b13671787d8b18e7ece5
egomezbpedro <egomezbpedro@gmail.com>
2024-06-14 11:43:17 +0200

    Adding shortcut to the packer file

--- lua/egomezbpedro/remap.lua
+++ lua/egomezbpedro/remap.lua
@@ -62,7 +62,7 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>K", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>J", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/egomezbpedro/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
